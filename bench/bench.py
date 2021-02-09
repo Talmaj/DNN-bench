5de17f5dd1c9e6f5f0c982792bdbda7b041440a7
Talmaj Marinc <talmaj@gmail.com>
2021-02-09 12:57:55 +0100

    Add complete run measurements data to results.

--- bench/bench.py
+++ bench/bench.py
@@ -2,6 +2,7 @@ import timeit
 
 
 def benchmark_speed(benchmark_func, repeat=1000, number=1, warmup=100):
+    assert repeat >= 2 * warmup, "Warmup should be at leat 2x smaller than repeat."
     out = timeit.repeat(benchmark_func, repeat=repeat, number=number)
 
     # remove warmup
@@ -10,4 +11,4 @@ def benchmark_speed(benchmark_func, repeat=1000, number=1, warmup=100):
     # calculate statistics
     mean = sum(out) / len(out)
     std = (sum((x - mean) ** 2 for x in out) / len(out)) ** 0.5
-    return dict(min=min(out), max=max(out), mean=mean, std=std)
+    return dict(min=min(out), max=max(out), mean=mean, std=std, data=out)
