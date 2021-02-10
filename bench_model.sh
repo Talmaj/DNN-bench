ae7b779f785babedc7d9a5c5c4af95a3b6a65f2b
Talmaj Marinc <talmaj@gmail.com>
2021-02-10 12:02:44 +0100

    Update bench_model.sh to split between different devices.

--- bench_model.sh
+++ bench_model.sh
@@ -13,6 +13,8 @@ do
               --openvino)        openvino=1 ;;
               --nuphar)          nuphar=1 ;;
               --tvm)             tvm=1 ;;
+              --ort-cuda)        ort_cuda=1 ;;
+              --ort-tensorrt)    ort_tensorrt=1 ;;
               --quantize)        quantize=1 ;;
               --repeat)          repeat=${VALUE} ;;
               --number)          number=${VALUE}  ;;
@@ -38,42 +40,90 @@ name=$(echo $(basename $model) | cut -f1 -d .)
 
 cwd=$(pwd)
 
-if [[ -n $pytorch ]]; then
-  echo PyTorch
-  docker run --rm -v $cwd/bench:/bench -v $model:$model -v $output:$output pytorch:latest \
-  python3 /bench $model --backend pytorch --repeat $repeat --number $number --warmup $warmup \
-  --backend-meta onnx2pytorch --device $device --quantize $quantize \
-  --output-path $output/"$name"-quant="$quantize"-pytorch-onnx2pytorch.json
-fi
+if [[ $device == "cpu" ]]; then
+  if [[ -n $pytorch ]]; then
+    echo PyTorch
+    docker run --rm -v $cwd/bench:/bench -v $model:$model -v $output:$output pytorch:latest \
+    python3 /bench $model --backend pytorch --repeat $repeat --number $number --warmup $warmup \
+    --backend-meta onnx2pytorch --device $device --quantize $quantize \
+    --output-path $output/"$name"-quant="$quantize"-"$device"-pytorch-onnx2pytorch.json
+  fi
 
-if [[ -n $onnxruntime ]]; then
-  echo OpenMP
-  docker run --rm -v $cwd/bench:/bench -v $model:$model -v $output:$output onnxruntime:latest \
-  python3 /bench $model --backend onnxruntime --repeat $repeat --number $number --warmup $warmup \
-  --backend-meta openmp --device $device --quantize $quantize \
-  --output-path $output/"$name"-quant="$quantize"-onnxruntime-openmp.json
-fi
+  if [[ -n $onnxruntime ]]; then
+    echo OpenMP
+    docker run --rm -v $cwd/bench:/bench -v $model:$model -v $output:$output onnxruntime:latest \
+    python3 /bench $model --backend onnxruntime --repeat $repeat --number $number --warmup $warmup \
+    --backend-meta openmp --device $device --quantize $quantize \
+    --output-path $output/"$name"-quant="$quantize"-"$device"-onnxruntime-openmp.json
+  fi
 
-if [[ -n $openvino ]]; then
-  echo OpenVino
-  docker run --rm -v $cwd/bench:/bench -v $model:$model -v $output:$output mcr.microsoft.com/azureml/onnxruntime:latest-openvino-cpu \
-  python3 /bench $model --backend onnxruntime --repeat $repeat --number $number --warmup $warmup \
-  --backend-meta openvino --device $device --quantize $quantize \
-  --output-path $output/"$name"-quant="$quantize"-onnxruntime-openvino.json
-fi
+  if [[ -n $openvino ]]; then
+    echo OpenVino
+    docker run --rm -v $cwd/bench:/bench -v $model:$model -v $output:$output mcr.microsoft.com/azureml/onnxruntime:latest-openvino-cpu \
+    python3 /bench $model --backend onnxruntime --repeat $repeat --number $number --warmup $warmup \
+    --backend-meta openvino --device $device --quantize $quantize \
+    --output-path $output/"$name"-quant="$quantize"-"$device"-onnxruntime-openvino.json
+  fi
 
-if [[ -n $tvm || -n $nuphar ]]; then
-  echo Nuphar
-  docker run --rm -v $cwd/bench:/bench -v $model:$model -v $output:$output mcr.microsoft.com/azureml/onnxruntime:latest-nuphar \
-  python3 /bench $model --backend onnxruntime --repeat $repeat --number $number --warmup $warmup \
-  --backend-meta nuphar --device $device --quantize $quantize \
-  --output-path $output/"$name"-quant="$quantize"-onnxruntime-nuphar.json
-fi
+  if [[ -n $tvm || -n $nuphar ]]; then
+    echo Nuphar
+    docker run --rm -v $cwd/bench:/bench -v $model:$model -v $output:$output mcr.microsoft.com/azureml/onnxruntime:latest-nuphar \
+    python3 /bench $model --backend onnxruntime --repeat $repeat --number $number --warmup $warmup \
+    --backend-meta nuphar --device $device --quantize $quantize \
+    --output-path $output/"$name"-quant="$quantize"-"$device"-onnxruntime-nuphar.json
+  fi
 
-if [[ -n $tf ]]; then
-  echo TensorFlow
-  docker run --rm -v $cwd/bench:/bench -v $model:$model -v $output:$output tensorflow:latest \
-  python3 /bench $model --backend tf --repeat $repeat --number $number --warmup $warmup \
-  --backend-meta onnx_tf --device $device --quantize $quantize \
-  --output-path $output/"$name"-quant="$quantize"-tf-onnx_tf.json
+  if [[ -n $tf ]]; then
+    echo TensorFlow
+    docker run --rm -v $cwd/bench:/bench -v $model:$model -v $output:$output tensorflow:latest \
+    python3 /bench $model --backend tf --repeat $repeat --number $number --warmup $warmup \
+    --backend-meta onnx_tf --device $device --quantize $quantize \
+    --output-path $output/"$name"-quant="$quantize"-"$device"-tf-onnx_tf.json
+  fi
+
+
+elif [[ $device == "gpu" ]]; then
+    if [[ -n $pytorch ]]; then
+    echo PyTorch
+    nvidia-docker run --rm -v $cwd/bench:/bench -v $model:$model -v $output:$output pytorch:latest \
+    python3 /bench $model --backend pytorch --repeat $repeat --number $number --warmup $warmup \
+    --backend-meta onnx2pytorch --device $device --quantize $quantize \
+    --output-path $output/"$name"-quant="$quantize"-"$device"-pytorch-onnx2pytorch.json
+  fi
+
+  if [[ -n $ort_cuda ]]; then
+    echo onnxruntime-cuda
+    nvidia-docker run --rm -v $cwd/bench:/bench -v $model:$model -v $output:$output onnxruntime:latest-cuda \
+    python3 /bench $model --backend onnxruntime --repeat $repeat --number $number --warmup $warmup \
+    --backend-meta cuda --device $device --quantize $quantize \
+    --output-path $output/"$name"-quant="$quantize"-"$device"-ort-cuda.json
+  fi
+
+  if [[ -n $ort_tensorrt ]]; then
+    echo onnxruntime-tensorrt
+    nvidia-docker run --rm -v $cwd/bench:/bench -v $model:$model -v $output:$output onnxruntime:latest-tensorrt \
+    python3 /bench $model --backend onnxruntime --repeat $repeat --number $number --warmup $warmup \
+    --backend-meta tensorrt --device $device --quantize $quantize \
+    --output-path $output/"$name"-quant="$quantize"-"$device"-ort-tensorrt.json
+  fi
+
+  if [[ -n $tf ]]; then
+    echo TensorFlow - $device
+    nvidia-docker run --rm -v $cwd/bench:/bench -v $model:$model -v $output:$output tensorflow:latest-gpu \
+    python3 /bench $model --backend tf --repeat $repeat --number $number --warmup $warmup \
+    --backend-meta onnx_tf --device $device --quantize $quantize \
+    --output-path $output/"$name"-quant="$quantize"-"$device"-tf-onnx_tf.json
+  fi
+
+
+elif [[ $device == "arm" ]]; then
+    if [[ -n $onnxruntime ]]; then
+      echo onnxruntime-arm
+      docker run --rm -v $cwd/bench:/bench -v $model:$model -v $output:$output onnxruntime:arm64v8 \
+      python3 /bench $model --backend onnxruntime --repeat $repeat --number $number --warmup $warmup \
+      --backend-meta arm64v8 --device $device --quantize $quantize \
+      --output-path $output/"$name"-quant="$quantize"-"$device"-onnxruntime-arm64v8.json
+    fi
+else
+  echo "Specify one of the following devices: cpu, gpu or arm."
 fi
